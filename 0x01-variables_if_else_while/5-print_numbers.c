277462ff25d681a141ac7076c9fed2e357b2ca63
Festus <adebayofestusbamidele@gmail.com>
2023-03-17 21:32:23 -0700

    0x01-variables_if_else_while mandatory task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print set of integer
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
