a53ba6a4c003800fc16ddc8a5f94f280113c291f
Festus <adebayofestusbamidele@gmail.com>
2023-03-20 07:20:33 -0700

    mandatory tasks

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,19 +15,19 @@ int main(void)
 	int d;
 	int e = 0;
 
-	while (e <= 9)
+	while (e < 10)
 	{
 		d = 0;
-		while (d <= 9)
+		while (d < 10)
 		{
 			c = 0;
-			while (c <= 9)
+			while (c < 10)
 			{
 				if (c != d && d != e && e < d && d < c)
 				{
-					putchar('48' + e);
-					putchar('48' + d);
-					putchar('48' + c);
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
 
 					if (c + d + e != 9 + 8 + 7)
 					{
