0edda2d29808b99b301bb6de73afa6ef18e89ca7
Festus <adebayofestusbamidele@gmail.com>
2023-03-21 07:12:29 -0700

    mandatory tasks

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all natural numbers from
+ * @n: number to start from
+ * Return: 1 0r 0
+*/
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  _abs - computes the absolute value of an integer
+ *  @n: the int in question
+ *
+ *  Return: The abs of the int
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
